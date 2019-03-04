e3ac84dd634f6ccaa107e0345a1bb0dd3a6ca170
ZeroMaxinumXZ <that.coder.with.cake@gmail.com>
2019-03-03 20:36:31 -0600

    Add files via upload

--- actions.py
+++ actions.py
@@ -10,32 +10,26 @@ def bounds(action, bound1, bound2):
 
 def actionizer(act_click, act_vert, act_horiz, reward = 0, prev_x = None, prev_y = None):
     if act_click <= 1000:
-        reward += 0.01
+        #reward += 0.01
         click()
         print("Agent has clicked.")
     else:
-        reward += 0
-    if act_vert <= 980 and act_vert >= 100:
-        y = act_vert
-        print("Agent will move to y: " + str(y))
-        reward += 0.001
-        fakey = y
-    else:
-        print("Agent will do nothing in y.") 
-        reward = -1
-        y = None
-        fakey = -1
+        temp = 0
+        #reward -= 0.01
     if act_horiz <= 1920:
         x = act_horiz
         print("Agent will move to x: " + str(x))
-        reward += 0.001
+        #reward += 0.001
         fakex = x
     else:
         print("Agent will do nothing in x.")
-        reward = -1
+        #reward = -1
         x = None
         fakex = -1
 
+    y = act_vert // 2
+    print("Agent will move to y: " + str(y))
+    fakey = y
 
     moveTo(x, y, 1)
     return reward, fakex, fakey
