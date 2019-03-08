613f178883b32bca289a3a3a4ad81b400769f033
ZeroMaxinumXZ <that.coder.with.cake@gmail.com>
2019-03-08 11:26:41 -0600

    v1.1.1

--- algernon/actions.py
+++ algernon/actions.py
@@ -1,6 +1,8 @@
 from pyautogui import click
 from pyautogui import moveTo
 from pyautogui import position, press
+from math import ceil
+from utils import act_screen_size
 
 def bounds(action, bound1, bound2):
     if action >= bound1 and action <= bound2:
@@ -9,14 +11,14 @@ def bounds(action, bound1, bound2):
          return False
 
 def actionizer(act_click, act_vert, act_horiz, act_type, reward = 0, prev_x = None, prev_y = None):
-    if act_click <= 1000:
+    if act_click <= ceil(act_screen_size()[1]) // 2:
         #reward += 0.01
         click()
         print("Agent has clicked.")
     else:
         temp = 0
         #reward -= 0.01
-    if act_horiz <= 1920:
+    if act_horiz <= act_screen_size()[1]:
         x = act_horiz
         print("Agent will move to x: " + str(x))
         #reward += 0.001
@@ -26,17 +28,19 @@ def actionizer(act_click, act_vert, act_horiz, act_type, reward = 0, prev_x = No
         #reward = -1
         x = None
         fakex = -1
-    if act_type <= 333:
+        
+    screen_type_act_num = act_screen_size()[1] // 6
+    if act_type <= screen_h:
         press('command')
-    elif act_type <= 666:
+    elif act_type <= screen_h * 2:
         press('up')
-    elif act_type <= 999:
+    elif act_type <= screen_h * 3:
         press('left')
-    elif act_type <= 1332:
+    elif act_type <= screen_h * 4:
         press('right')
-    elif act_type <= 1665:
+    elif act_type <= screen_h * 5:
         press('down')
-    elif act_type <= 2000:
+    elif act_type <= screen_h * 6:
         pass
     y = act_vert // 2
     print("Agent will move to y: " + str(y))
