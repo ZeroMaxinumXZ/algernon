3cbedb8c08a91476f0a4fe0536c83a258b2bb25f
ZeroMaxinumXZ <that.coder.with.cake@gmail.com>
2019-03-08 01:30:16 -0600

    v1.1

--- reinfinity-base/start.py
+++ reinfinity-base/start.py
@@ -1,18 +1,19 @@
 from agent import *
 from utils import *
 from rewarders import *
-
 from os import system
+curiousity_engine = model_build()
+curiousity_engine.compile(optimizer = 'adam', loss='mean_squared_logarithmic_error')
+
 
 def main_loop(agent):
     system("clear")
     print("-" * 30) 
-    timer = 10
+    timer = 30
     
     print("Control-C to quit.")
-    print("Try moving to the top-left corner of screen if AI gets somewhere it shouldn't, lol... Restart your computer if it doesn't work.")
-    print("This is an experimental reinforcement-learning AI that controls mouse input.")
-    
+    print("This is Albert, an experimental reinforcement-learning AI that controls mouse input.")
+    print("Instructions for quitting: \nForce the mouse to the top-left corner of the screen,\nor just tap Control-C.")
     print("Timer in case you want to quit this program: ")
     while timer != 0:
         print(str(timer) + '...')
@@ -25,12 +26,11 @@ def main_loop(agent):
         action1 = agent.act(states=get_screen())
         action2 = agent.act(states=get_screen())
         action3 = agent.act(states=get_screen())
-        action_sum = action1 + action2 + action3
-        #action4 = agent.act(states=get_screen())
-        reward, x, y = actionizer(action1,action2, action3)
-        reward = smart_rewarder(reward, action_sum)
+        action4 = agent.act(states=get_screen())
+        x, y = actionizer(action1, action2, action3, action4)
+        reward = curiousity_rewarder(action1, action2, action3, action4, curiousity_engine)
         #rwward = object_rewarder(reward, img)
-        reward = xystoreandcheck(x, y, reward)
+        #reward = xystoreandcheck(x, y, reward)
         agent.observe(reward=reward, terminal=False)
         #agent.save_model(join(getcwd(), "models", "model"))
 
