87d86abeb3932be6e86134ff8867db116c8697c2
ZeroMaxinumXZ <that.coder.with.cake@gmail.com>
2019-03-08 01:31:20 -0600

    v1.1

--- reinfinity-base/rewarders.py
+++ reinfinity-base/rewarders.py
@@ -1,14 +1,26 @@
 from agent import *
 from utils import get_screen as env
 from random import uniform
-agent_rewards = smart_rewarder_build()
+import keras
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
 
-def smart_rewarder(rewards, actions = 0.01):
-    action = agent_rewards.act(states=env())
-    rewards += action - 5
-    print("Actor rewards: " + str(rewards))
-    SARrewards = (0.0001 * actions)
-    #SARrewards = 0
-    print("Smart rewarder's rewards: " + str(SARrewards))
-    agent_rewards.observe(reward=SARrewards, terminal=False)
+def model_build():
+    model = Sequential()
+    model.add(Flatten())
+    model.add(Dense(256 * 20, activation='sigmoid'))
+    model.add(Dense(256 * 10, activation='relu'))
+    model.add(Dense(256 * 5, activation='sigmoid'))
+    model.add(Dense(256 * 1, activation='sigmoid'))
+    model.add(Dense(4))
+    return model
+
+def curiousity_rewarder(action1, action2, action3, action4, model, rewards=0.0):
+    y = np.array([[action1, action2, action3, action4]])
+    history = model.fit(x=np.array([env()]), y = y)
+    loss = history.history['loss'][0]
+    rewards = (loss * .01)
     return rewards
+
+ 
